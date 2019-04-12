bcb80be62cf1144a5bc918aec51b64731a7ce59b
MaximLutay <maksym.lutai@nure.ua>
2019-04-12 22:56:42 +0300

    typo fix

--- serviceworker.js
+++ serviceworker.js
@@ -5,15 +5,15 @@ let URLS_TO_CACHE = ["/"];
 
 
 
-self.addEventListener("install", event => {
-    event.waitUntill(
+self.addEventListener("install", e => {
+    e.waitUntill(
         caches.open(CACHE_NAME)
         .then(cache =>  (console.log("Cache opened"),cache.addAll(URLS_TO_CACHE)) )
     );
 });
 
 self.addEventListener("fetch", function (e) {
-    e.respondWith(caches.match(event.request).then(req => req || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then(req => req || fetch(e.request)));
 });
 
 
